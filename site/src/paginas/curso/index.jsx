abea1628f967bf74d60afad25ce7b365124ad3d7
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-12-19 15:18:44 -0300

    re-upload

---
+++ site/src/paginas/curso/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Cabecalho } from '../../componentes/cabecalho'
+import { CadastroCurso } from './cadastro'
+
+export class CursoIndex extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Cabecalho titulo="Cursos" subtitulo="Cadastro de cursos" />
+                <CadastroCurso />
+            </div>
+        )
+    }
+}
